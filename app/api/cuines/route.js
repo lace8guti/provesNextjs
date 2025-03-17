9cfd308770c200a16f530034c555a065b581f501
Alumne Matí <alumne@localhost.localdomain>
2025-03-17 08:34:17 +0100

    api rest 2

--- app/api/cuines/route.js
+++ app/api/cuines/route.js
@@ -39,18 +39,3 @@ export async function POST(request) {
     }
 }
 
-// Afegir a models/cuina.js
-import { z } from "zod";
-
-export const cuinaValidator = z.object({
-  nom: z
-      .string({
-        required_error: "El nom és obligatori.", })
-      .min(1, { message: "El nom ha de tenir almenys 3 caràcters." }).trim(),
-  descripcio: z.string().min(10, { message: "La descripció ha de tenir almenys 10 caràcters." }).trim(),
-  nivellDificultat: z
-    .number()
-    .min(1, { message: "El nivell de dificultat ha de ser com a mínim 1." })
-    .max(10, { message: "El nivell de dificultat ha de ser com a màxim 10." }),
-  vegana: z.boolean().optional(), // No és obligatori
-});
