f2a9d88f69afedaa2c9c14cb546abbd654805e66
Alumne Matí <alumne@localhost.localdomain>
2025-03-18 10:48:19 +0100

    arreglat la cosa de mongodb

--- seeders/cuines.js
+++ seeders/cuines.js
@@ -6,8 +6,8 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
   
 const cuines = [
-  { nom: 'Mediterrània', descripcio: "Cuina saludable amb oli d'\'oliva, verdures i peix.", nivellDificultat: 5, vegana: false },
-  { nom: 'Asiàtica', descripcio: 'Gran varietat de sabors i tècniques de cuina tradicionals d'\'Àsia.', nivellDificultat: 7, vegana: true },
+  { nom: 'Mediterrània', descripcio: "Cuina saludable amb oli d'oliva, verdures i peix.", nivellDificultat: 5, vegana: false },
+  { nom: 'Asiàtica', descripcio: "Gran varietat de sabors i tècniques de cuina tradicionals d'Àsia.", nivellDificultat: 7, vegana: true },
   { nom: 'Francesa', descripcio: 'Alta cuina amb tècniques elaborades i ingredients sofisticats.', nivellDificultat: 8, vegana: false },
   { nom: 'Mexicana', descripcio: 'Sabor intens amb espècies, xili i tortillas.', nivellDificultat: 6, vegana: false },
   { nom: 'Italiana', descripcio: 'Pasta, pizzes i plats tradicionals plens de sabor.', nivellDificultat: 4, vegana: true },
